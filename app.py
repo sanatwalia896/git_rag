3748c1ead7080c54e4024d605130c924dca7053d
sanatwalia896 <121603801+sanatwalia896@users.noreply.github.com>
2025-01-18 18:40:26 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ from gitingest import ingest
 
 import langchain
 from langchain_community.document_loaders import GitLoader
+from langchain_groq import ChatGroq
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+import streamlit as st
